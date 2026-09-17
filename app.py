3e9f8d50c15cbf47523613a678030b0c7bc59a71
Joel Wassberg <joel.wassberg@skola.taby.se>
2026-09-17 13:24:43 +0200

    Gjort en bas. Alltså nu kan man skriva och det sparas och skrivs up på html sidan

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, redirect, request, render_template_string
+import os, json
+
+app = Flask(__name__)
+
+FILE_PATH = 'media.data'
+
+
+def read_data():
+    if os.path.exists(FILE_PATH):
+        with open(FILE_PATH, 'r') as file:
+            data = json.load(file)
+    else:
+        data = []
+    return data
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template_string(open('templates/media.html').read(), media=json.dumps(read_data(), ensure_ascii=False))
+
+
+@app.route('/text-channel', methods=['POST'])
+def write():
+    media = read_data()
+    media.append({
+        'name': request.form.get('name', ''),
+        'message': request.form.get('message', '')
+    })
+    with open(FILE_PATH, 'w') as file:
+        json.dump(media, file, indent=2)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
